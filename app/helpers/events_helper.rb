cb97e24df8a889a7770898e216f9a97e7d3438ad
Jeff Roush <jeff@jeffroush.com>
2018-10-28 16:06:15 -0700

    Handle running events

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -3,6 +3,14 @@ module EventsHelper
     "%.2f" % (minutes/60.0)
   end
 
+  def formatted_duration_seconds(seconds)
+    if seconds
+      "%.2f hours" % (seconds/3600.0)
+    else
+      "running"
+    end
+  end
+
   def formatted_time(time)
     time.strftime("%Y-%m-%d %I:%M")
   end
